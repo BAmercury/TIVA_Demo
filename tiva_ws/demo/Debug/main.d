d4e2144a3140379c74c2695a3cbac67acfe51acb
BrianAmin <tuf49787@temple.edu>
2019-04-19 18:15:36 -0400

    Learning to use the UART interface

--- tiva_ws/demo/Debug/main.d
+++ tiva_ws/demo/Debug/main.d
@@ -11,9 +11,16 @@ main.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine
 main.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h
 main.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h
 main.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h
 main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/debug.h
 main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h
 main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/fpu.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/uart.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.h
+main.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h
 
 ../main.c: 
 C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h: 
@@ -26,6 +33,13 @@ C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h
 C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h: 
 C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h: 
 C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h: 
 C:/ti/tivaware_c_series_2_1_4_178/driverlib/debug.h: 
 C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h: 
 C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/fpu.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/uart.h: 
+C:/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h: 
--- tiva_ws/demo/main.c
+++ tiva_ws/demo/main.c
@@ -1,10 +1,15 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
 #include "driverlib/debug.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
-
+#include "driverlib/fpu.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
 // The error routine that is called if the driver library encounters an error.
 //
 //*****************************************************************************
@@ -22,6 +27,23 @@ __error__(char *pcFilename, uint32_t ui32Line)
  */
 int main(void)
 {
+    // Enable the GPIO Peripheral used for the UART
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    // Enable UART0
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    // Configure GPIO Pins for UART Mode
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    // Use the internal 16 Mhz oscillator as the UART Clock Source
+    // We need a clock source for the baud rate generator
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+    // Initialize the UART for console IO
+    UARTStdioConfig(0, 115200, 16000000);
 
 
     // Enable GPIO port used for on-board LED
@@ -34,21 +56,40 @@ int main(void)
     // Enable the GPIO Pins of Port F, Pin 1 is R, Pin 2 is G, Pin 3 is B
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
 
+    // Print some text
+
+    //
+    // Enable lazy stacking for interrupt handlers.  This allows floating-point
+    // instructions to be used within interrupt handlers, but at the expense of
+    // extra stack usage.
+    //
+    FPULazyStackingEnable();
+
+    // Set the clocking to run directly from the crystal
+    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ |SYSCTL_OSC_MAIN);
+
+
+
+    // Hello!
+    UARTprintf("Hello dude\n");
+
+
+
     // Main Loop
     while(1)
     {
         // Turn on the Red LED
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_1);
         // Delay for a bit
-        SysCtlDelay(2000000);
+        SysCtlDelay(SysCtlClockGet() / 10 / 3);
         // Turn on the Green
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_2);
         // Delay
-        SysCtlDelay(2000000);
+        SysCtlDelay(SysCtlClockGet() / 10 / 3);
         // Turn on the Blue
         GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_3);
         // Delay
-        SysCtlDelay(2000000);
+        SysCtlDelay(SysCtlClockGet() / 10 / 3);
     }
 
 
